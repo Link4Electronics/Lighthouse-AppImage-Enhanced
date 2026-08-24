c2f12e05e934ca1ea3178f4adb7b184a3729ee57
Link4Electronics <link4electronics@gmail.com>
2026-08-24 18:51:34 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -41,8 +41,5 @@ cmake . \
 cmake --build build --config Release
 cmake --build build --config Release --target GeneratePortO2R
 
-mv -v build/assets ../AppDir/bin
-mv -v build/Lighthouse ../AppDir/bin
-mv -v build/config.yml ../AppDir/bin
-mv -v build/lighthouse.o2r ../AppDir/bin
+mv -v build/assets build/Lighthouse build/config.yml build/lighthouse.o2r ../AppDir/bin
 wget -O ../AppDir/bin/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
