1b136a6d67083125564e06f45dc668262e2e4200
Link4Electronics <link4electronics@gmail.com>
2026-08-24 18:13:06 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -14,8 +14,6 @@ export DEPLOY_OPENGL=1
 # Deploy dependencies
 quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
 
-# Additional changes can be done in between here
-
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
 
